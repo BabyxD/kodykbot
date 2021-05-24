f25830564555078f338adb53d5887873cc611351
Baby <69705110+BabyxD@users.noreply.github.com>
2021-05-24 13:06:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    BOT_TOKEN = os.getenv("1830317530:AAGnuo33glzMAtflIWAp8Tn2KwSE_93L3Fs")
 JSMAPI= "https://jiosaavnapi.bhadoo.uk/result/?query=" 
